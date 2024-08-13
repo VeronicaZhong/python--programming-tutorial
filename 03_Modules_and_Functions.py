8cd0a449012f811f8893705e8c543b269566dcb6
VeronicaZhong <veronicazhong0813@gmail.com>
2024-08-13 16:39:01 +0100

    03_Modules_and_Functions

---
+++ 03_Modules_and_Functions.py
@@ -0,0 +1,24 @@
+Last login: Tue Aug 13 16:27:09 on ttys000
+veronica@vv-macair ~ % python3
+Python 3.12.4 (main, Jun  6 2024, 18:26:44) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> 5 ** 4
+625
+>>> pow(5,4)
+625
+>>> abs(-18)
+18
+>>> abs(5)
+5
+>>> import math
+>>> math.floor(18.7)
+18
+>>> math.sqrt(81)
+9.0
+>>> buckey = math.sqrt
+>>> buckey(9)
+3.0
+>>> buckey = math.floor
+>>> buckey(19.8)
+19
+>>> 
