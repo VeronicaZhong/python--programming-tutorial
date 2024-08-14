1a1ef59ca86431ff4bdd2c600caef63d9709400f
VeronicaZhong <veronicazhong0813@gmail.com>
2024-08-14 16:40:42 +0100

    More_on_Strings

---
+++ 06_More on Strings.py
@@ -0,0 +1,14 @@
+>>> "hey now"
+'hey now'
+>>> print("hey now")
+hey now
+>>> num = 18
+>>> num + 16
+34
+>>> num = str(18)
+>>> print("buckey is " + num)
+buckey is 18
+>>> num2 = str(32)
+>>> print("my mom is " + num2)
+my mom is 32
+>>> repr()
