bddae64c9bcb9639b92ba2214aee4394711807d9
VeronicaZhong <veronicazhong0813@gmail.com>
2024-08-14 16:27:53 +0100

    Strings

---
+++ 05_Strings.py
@@ -0,0 +1,22 @@
+veronica@vv-macair ~ % python3
+Python 3.12.4 (main, Jun  6 2024, 18:26:44) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> "hey now"
+'hey now'
+>>> "he's a jerk"
+"he's a jerk"
+>>> 'he\'s a jerk'
+"he's a jerk"
+>>> "buckey said,\"hey now\" to me"
+'buckey said,"hey now" to me'
+>>> "let's go fishing"
+"let's go fishing"
+>>> a = "buckey"
+>>> b = "roberts"
+>>> a + b
+'buckeyroberts'
+>>> a,b
+('buckey', 'roberts')
+>>> a = "buckey "
+>>> a + b
+'buckey roberts'
