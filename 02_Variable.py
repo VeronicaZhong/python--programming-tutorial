83cb6ec42d3561758e73fc540ffb469d6bcaf615
VeronicaZhong <veronicazhong0813@gmail.com>
2024-08-17 00:50:31 +0100

    UpdateVariable.py
    Variable

--- 02_Variable.py
+++ 02_Variable.py
@@ -1,4 +1,3 @@
-veronica@vv-macair ~ % python3
 Python 3.12.4 (main, Jun  6 2024, 18:26:44) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
 Type "help", "copyright", "credits" or "license" for more information.
 >>> x = 18
