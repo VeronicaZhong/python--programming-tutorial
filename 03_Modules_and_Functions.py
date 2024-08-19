929e40c9f5037e4afb1c62a1f03c649b9ed7ea52
VeronicaZhong <veronicazhong0813@gmail.com>
2024-08-19 20:07:12 +0100

    Update 03_Modules_and_Functions.py
    Modules and Functions

--- 03_Modules_and_Functions.py
+++ 03_Modules_and_Functions.py
@@ -1,4 +1,4 @@
-Python 3.12.4 (main, Jun  6 2024, 18:26:44) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
+main, Jun  6 2024, 18:26:44) [Clang 15.0.0 (clang-1500.3.9.4)] on darwin
 Type "help", "copyright", "credits" or "license" for more information.
 >>> 5 ** 4
 625
